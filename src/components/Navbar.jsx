8ab19a9fec0ddf267edbb362ebfc51a7871500e8
robertmarinescu <robert.marinescu93@gmail.com>
2022-02-01 19:09:07 +0200

    implement Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { 
+  HomeOutlined, 
+  MoneyCollectOutlined, 
+  BulbOutlined, 
+  FoundOutlined, 
+  MenuOutlined 
+} from '@ant-design/icons'
+import icon from '../images/cryptocurrency.png'
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+      <div className='logo-container'>
+        <Avatar src={icon} size='large'/>
+        <Typography.Title level={2} className='logo'>
+          <Link to='/'>Cryptoverse</Link>
+        </Typography.Title>
+        {/* <Button className='menu-control-container'>
+
+        </Button> */}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
