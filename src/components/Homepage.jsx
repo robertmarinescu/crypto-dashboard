b260b2214a9d6735bf002a4021c3f7dec4893087
robertmarinescu <robert.marinescu93@gmail.com>
2022-02-02 18:18:07 +0200

    setup store

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom'
+import { FormProvider } from 'antd/lib/form/context';
+
+const { Title } = Typography
 
 const Homepage = () => {
   return (
-    <div>
-      Homepag
-    </div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title='Total Cryptocurrencies' value='5'/></Col>
+        <Col span={12}><Statistic title='Total Exchanges' value='5'/></Col>
+        <Col span={12}><Statistic title='Total Market Cap' value='5'/></Col>
+        <Col span={12}><Statistic title='Total 24h Volumes' value='5'/></Col>
+        <Col span={12}><Statistic title='Total Markets' value='5'/></Col>
+      </Row>
+    </>
   )
 }
 
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+var options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  // params: {
+  //   referenceCurrencyUuid: 'yhjMzLPhuIDl',
+  //   timePeriod: '24h',
+  //   tiers: '1',
+  //   orderBy: 'marketCap',
+  //   orderDirection: 'desc',
+  //   limit: '50',
+  //   offset: '0'
+  // },
+  headers: {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '29547ae383msh514cf083aeade2dp141c83jsnaeff3bab45c0'
+  }
+};
