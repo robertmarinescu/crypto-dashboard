a103e0c230e01305c5a1fb109204d85421af4997
robertmarinescu <robert.marinescu93@gmail.com>
2022-02-02 18:31:50 +0200

    setup the service

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,17 +1,20 @@
-var options = {
-  method: 'GET',
-  url: 'https://coinranking1.p.rapidapi.com/coins',
-  // params: {
-  //   referenceCurrencyUuid: 'yhjMzLPhuIDl',
-  //   timePeriod: '24h',
-  //   tiers: '1',
-  //   orderBy: 'marketCap',
-  //   orderDirection: 'desc',
-  //   limit: '50',
-  //   offset: '0'
-  // },
-  headers: {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '29547ae383msh514cf083aeade2dp141c83jsnaeff3bab45c0'
-  }
-};
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/exchanges')
+    })
+  })
+})
