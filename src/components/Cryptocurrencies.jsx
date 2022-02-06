1dc57c0a3402faeb5a37758bad6a2592704b0f09
robertmarinescu <robert.marinescu93@gmail.com>
2022-02-06 18:59:13 +0200

    add details for each crypto news and implement Select option to filter the news after a specific coin

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -24,7 +24,7 @@ const Cryptocurrencies = ({ simplified }) => {
           <Input placeholder='Search Cryptocurrency' onChange={e => setSearchTerm(e.target.value)}/>
         </div>
       }
-      <Row gutter={[32, 32]} className='crypto-card-container'>
+      <Row gutter={[24, 24]} className='crypto-card-container'>
         {cryptos?.map((currency) =>(
           <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
             <Link to={`/crypto/${currency.id}`}>
