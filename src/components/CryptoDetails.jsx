c509cc2464439186b18d849dedd41d79925145a5
robertmarinescu <robert.marinescu93@gmail.com>
2022-02-08 19:00:45 +0200

    fix getCryptoHistory in order to get the correct data

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -26,10 +26,13 @@ const CryptoDetails = () => {
   const { coinId } = useParams()
   const [timePeriod, setTimePeriod] = useState('7d')
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
-  const { data: coinHistory } = useGetCryptoDetailsQuery(coinId, timePeriod)
+  const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timePeriod })
   const cryptoDetails = data?.data?.coin
 
   console.log(cryptoDetails)
+  console.log(coinHistory)
+
+  if(isFetching) return 'Loading...'
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
@@ -67,8 +70,12 @@ const CryptoDetails = () => {
         onChange={(value) => setTimePeriod(value)}
       >
         {time.map((date) => <Option key={date}>{date}</Option>)}
-      <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails?.name}/>
       </Select>
+      <LineChart
+        coinHistory={coinHistory} 
+        currentPrice={millify(cryptoDetails.price)} 
+        coinName={cryptoDetails?.name}
+      />
       <Col className='stats-container'>
         <Col className='coin-value-statistics'>
           <Col className='coin-value-statistics-heading'>
@@ -111,10 +118,10 @@ const CryptoDetails = () => {
       </Col>
       <Col className='coin-desc-link'>
           <Row className='coin-desc'>
-            {/* <Title level={3} className='coin-details-heading'>
+            <Title level={3} className='coin-details-heading'>
               What is {cryptoDetails?.name}
               {HTMLReactParser(cryptoDetails?.description)}
-            </Title> */}
+            </Title>
           </Row>
           <Col className='coin-links'>
             <Title level={3} className='coin-details-heading'>
